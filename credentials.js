2a99d0ddf7c0ee6d73eef4327ccb49230fe3d8b9
Mark Watkins <mark.watkins1@gmail.com>
2020-01-17 12:36:45 -1000

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,68 +1,78 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 chrome.runtime.onMessage.addListener(
-  function (message, sender, sendResponse) {
-    if (message.urlList) {
-      urlList = message.urlList;
-      i = 1;
-      move(1,urlList.length);
-      chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
-        chrome.tabs.update(tab.id, { url: urlList[0] });
-      });
-      recipes = [];
-      console.log(message);
-    } else if (message.recipe) {
-      var found = recipes.some(function (el) {
-        return el.sourceURL === message.recipe.sourceURL;
-      });
-      if (!found) {
-        recipes.push(message.recipe);
-        if (i < urlList.length) {
-       // if(i<3){  
-            move(i,urlList.length);
-          chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
-            chrome.tabs.update(tab.id, { url: urlList[i]});
-            i++;
-          });
-        } else {
-          jsonArray = JSON.stringify(recipes);
-          var blob = new Blob([jsonArray], {type: "application/json"});
-          var url = URL.createObjectURL(blob);
-          chrome.downloads.download({
-            url: url, // The object URL can be used as download URL
-            filename: 'pepperplateExport.json'
-          });
-          console.log(recipes);
-        }
-      }
+    async function (message, sender, sendResponse) {
+	console.log(message);
+
+	    
+	    if (message.urlList) {
+		urlList = message.urlList;
+		i = 1;
+		move(1,urlList.length);
+		chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
+		    chrome.tabs.update(tab.id, { url: urlList[0] });
+		});
+		recipes = [];
+		console.log(message);
+	    } else if (message.recipe) {
+		var found = recipes.some(function (el) {
+		    return el.sourceURL === message.recipe.sourceURL;
+		});
+		if (!found) {
+		    await sleep(2000).then(function () {
+			console.log('processing', message.recipe);
+			recipes.push(message.recipe);
+			if (i < urlList.length) {
+			    // if(i<3){  
+			    move(i,urlList.length);
+			    chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
+				chrome.tabs.update(tab.id, { url: urlList[i]});
+				i++;
+				
+			    });
+			} else {
+			    jsonArray = JSON.stringify(recipes);
+			    var blob = new Blob([jsonArray], {type: "application/json"});
+			    var url = URL.createObjectURL(blob);
+			    chrome.downloads.download({
+				url: url, // The object URL can be used as download URL
+				filename: 'pepperplateExport.json'
+			    });
+			    console.log(recipes);
+			}
+		    });
+		}
+	    }
     }
-  }
 );
 
 chrome.tabs.onUpdated.addListener(function () {
-  initApp();
+    initApp();
 });
 
 window.onload = function () {
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
-    chrome.tabs.update(tab.id, { url: 'http://www.pepperplate.com/recipes/default.aspx?123' });
-  });
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tab) {
+	chrome.tabs.update(tab.id, { url: 'https://www.pepperplate.com/recipes/default.aspx?123' });
+    });
 };
 
 
 /**
-FUNCTIONS
- */
+   FUNCTIONS
+*/
 function initApp() {
-  chrome.tabs.executeScript(null, {
-    file: "getRecipe.js",
-    allFrames: false
-  }, function () { });
+    chrome.tabs.executeScript(null, {
+	file: "getRecipe.js",
+	allFrames: false
+    }, function () { });
 
 }
 
 function move(current,max) {
-  var elem = document.getElementById("myBar"); 
-  width=Math.round((current/max)*100);
-  elem.style.width = width + '%'; 
-  elem.innerHTML = width * 1 + '%';
+    var elem = document.getElementById("myBar"); 
+    width=Math.round((current/max)*100);
+    elem.style.width = width + '%'; 
+    elem.innerHTML = width * 1 + '%';
 }
